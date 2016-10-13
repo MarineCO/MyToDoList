afdb1bb9363b12d2c16266d1c02601ca7323003e
marineco <marinecolonge@gmail.com>
2016-10-13 23:45:50 +0200

    Fonctionnalité tâches à faire disparaissent : check

--- ToDo.js
+++ ToDo.js
@@ -4,13 +4,25 @@ console.log('checkbox');
 
 //Click Add
 
-	$('#addTasksBtn').on('click', function(){
+$('#addTasksBtn').on('click', function(){
 
 	console.log('hello');
 
 	var addTask = $('input[name=addTask]').val();
 
 	$('.list').append('<li>' + '<input type="checkbox" class="task">' + addTask + '</li>')
+
+});
+
+
+//Click hide tasks to do
+
+	$('#hideTasksToDoBtn').click(function(){
+
+		console.log('ok');
+
+		$('input[type=checkbox]:not(:checked)').parent('li').hide();
+		$('input[type=checkbox]:checked').parent('li').show();
 
 	});
 
