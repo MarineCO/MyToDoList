73d1073bbe2b52cba9173cba7633fae8c18b734e
marineco <marinecolonge@gmail.com>
2016-10-31 18:20:47 +0100

    mise en place du css avec semantic ui - manque btn supprimer une tâche puis éditer une tâche

--- ToDo.js
+++ ToDo.js
@@ -1,48 +1,44 @@
-console.log('checkbox');
-
 (function main () {
 
 //Click Add
 
 $('#addTasksBtn').on('click', function(){
 
-	console.log('hello');
-
 	var addTask = $('input[name=addTask]').val();
 
-	$('.list').append('<li>' + '<input type="checkbox" class="task">' + addTask + '</li>')
+	$('.list').append('<li>' + '<input type="checkbox" class="task">' + ' ' +
+		addTask + '</li>' )
 
 });
 
 
 //Click hide tasks to do
 
-	$('#hideTasksToDoBtn').on('click', function(){
+$('#hideTasksToDoBtn').on('click', function(){
 
-		console.log('ok');
+	$('input[type=checkbox]:not(:checked)').parent('li').hide();
+	$('input[type=checkbox]:checked').parent('li').show();
 
-		$('input[type=checkbox]:not(:checked)').parent('li').hide();
-		$('input[type=checkbox]:checked').parent('li').show();
+});
 
-	});
+//Click hide tasks done
 
-	$('#hideTasksDoneBtn').on('click', function(){
+$('#hideTasksDoneBtn').on('click', function(){
 
-		console.log('ok2');
+	$('input[type=checkbox]:checked').parent('li').hide();
+	$('input[type=checkbox]:not(:checked)').parent('li').show();
 
-		$('input[type=checkbox]:checked').parent('li').hide();
-		$('input[type=checkbox]:not(:checked)').parent('li').show();
+});
 
-	});
+//Click show all tasks
 
-	$('#showAllTasksDoneBtn').on('click', function(){
+$('#showAllTasksDoneBtn').on('click', function(){
 
-		console.log('ok3');
+	$('input[type=checkbox]:checked').parent('li').show();
+	$('input[type=checkbox]:not(:checked)').parent('li').show();
 
-		$('input[type=checkbox]:checked').parent('li').show();
-		$('input[type=checkbox]:not(:checked)').parent('li').show();
+});
 
-	});
 
 
 })();
